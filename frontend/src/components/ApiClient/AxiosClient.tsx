4edf3c20e1b86433c9b5cba4996a7da7e132fd19
sparkels36 <piyush362002@gmail.com>
2025-06-30 11:43:38 +0530

    feat: change localhost backend url

--- frontend/src/components/ApiClient/AxiosClient.tsx
+++ frontend/src/components/ApiClient/AxiosClient.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/tasks",
+    baseURL: "https://taskmanager-snsn.onrender.com/tasks",
     headers: {
         "Content-Type": "application/json",
     },
